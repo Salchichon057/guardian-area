2af486b49b892c4e93d170c82be4799c9d2aff12
steve <stev8855@gmail.com>
2024-10-24 11:00:46 -0500

    feat: Add appRouter to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:guardian_area/config/config.dart';
+import 'package:guardian_area/config/router/app_router.dart';
 
 void main() async {
   await Environment.initEnvironment();
@@ -18,6 +19,7 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
+      routerConfig: appRouter,
       theme: AppTheme().getTheme(),
       debugShowCheckedModeBanner: false,
     );
