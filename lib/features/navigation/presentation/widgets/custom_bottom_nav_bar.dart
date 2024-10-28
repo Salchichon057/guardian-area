5a1209f820b635a686f53acedf4ec6dde0a7fbc9
steve <stev8855@gmail.com>
2024-10-28 15:07:56 -0500

    refactor: Update CustomBottomNavBar to improve navigation and styling

--- lib/features/navigation/presentation/widgets/custom_bottom_nav_bar.dart
+++ lib/features/navigation/presentation/widgets/custom_bottom_nav_bar.dart
@@ -15,20 +15,48 @@ class CustomBottomNavBar extends ConsumerWidget {
       currentIndex: navigationState.selectedIndex,
       onTap: (index) {
         navigationNotifier.updateIndex(index);
-
         if (index == 0) context.go('/');
         if (index == 1) context.go('/activities');
         if (index == 2) context.go('/chat');
         if (index == 3) context.go('/vital-signs');
-        if (index == 4) context.go('/geofences');        
-
+        if (index == 4) context.go('/geofences');
       },
+      
+      backgroundColor:
+          const Color(0xFF08273A),
+      selectedItemColor: Colors.white,
+      unselectedItemColor:
+          Colors.white54,
+      selectedLabelStyle: const TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.bold),
+      unselectedLabelStyle:
+          const TextStyle(fontSize: 12),
+      type: BottomNavigationBarType.fixed,
+
+      showUnselectedLabels: false,
+
       items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Activities'),
-        BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
-        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Vital signs'),
-        BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Geofences'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.notifications),
+          label: 'Activities',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat),
+          label: 'Chat',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite),
+          label: 'Vital signs',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          label: 'Geofences',
+        ),
       ],
     );
   }
