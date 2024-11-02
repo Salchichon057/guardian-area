335f52974004a47f23a8fec98160d498a007fd82
steve <stev8855@gmail.com>
2024-11-02 14:31:22 -0500

    feat: Integrate Firebase into the project with necessary configurations and initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:guardian_area/config/config.dart';
 import 'package:guardian_area/config/router/app_router.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await Environment.initEnvironment();
 
   runApp(
