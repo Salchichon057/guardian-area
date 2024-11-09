b5113aaa4d46932d2175d79e039d844c397f61a0
steve <stev8855@gmail.com>
2024-11-09 15:16:12 -0500

    feat: Update fetchGeofences method to accept selectedDeviceRecordId parameter

--- lib/features/geofences/domain/datasources/geofence_datasource.dart
+++ lib/features/geofences/domain/datasources/geofence_datasource.dart
@@ -2,5 +2,5 @@ import 'package:guardian_area/features/geofences/domain/entities/geofence.dart';
 
 abstract class GeofenceDatasource {
   Future<void> createGeofence(Geofence geofence);
-  Future<List<Geofence>> fetchGeofences();
+  Future<List<Geofence>> fetchGeofences(String selectedDeviceRecordId);
 }
