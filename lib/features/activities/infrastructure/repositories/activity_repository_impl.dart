c7f4227dfef7ff0b59a9e3b73c1d85f745a3306c
steve <stev8855@gmail.com>
2024-11-19 00:07:11 -0500

    feat: Implement ActivityRepositoryImpl for activity data fetching

---
+++ lib/features/activities/infrastructure/repositories/activity_repository_impl.dart
@@ -0,0 +1,15 @@
+import 'package:guardian_area/features/activities/domain/datasources/activity_datasource.dart';
+import 'package:guardian_area/features/activities/domain/entities/activity.dart';
+import 'package:guardian_area/features/activities/domain/repositories/activity_repository.dart';
+
+class ActivityRepositoryImpl extends ActivityRepository {
+  final ActivityDatasource datasource;
+
+  ActivityRepositoryImpl({required this.datasource});
+
+  @override
+  Future<List<Activity>> fetchActivities(
+      String selectedDeviceRecordId, String activityType) {
+    return datasource.fetchActivities(selectedDeviceRecordId, activityType);
+  }
+}
