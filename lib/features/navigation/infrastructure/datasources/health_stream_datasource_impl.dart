5d9e7374cfa06cf5f394dd05f11a04aa6929193f
steve <stev8855@gmail.com>
2024-11-18 20:46:28 -0500

    feat: Remove debug print statements from HealthStreamDatasourceImpl

--- lib/features/navigation/infrastructure/datasources/health_stream_datasource_impl.dart
+++ lib/features/navigation/infrastructure/datasources/health_stream_datasource_impl.dart
@@ -12,26 +12,20 @@ class HealthStreamDatasourceImpl {
     final uri = Uri.parse('$baseUrl/health-measures-stream?room=$roomId');
     _channel = WebSocketChannel.connect(uri);
 
-    print('Intentando conectar al WebSocket: $uri');
 
     return _channel!.stream.map((event) {
       try {
-        print('Evento recibido: $event');
         final jsonData = jsonDecode(event as String) as Map<String, dynamic>;
         return HealthMeasure.fromJson(jsonData);
       } catch (e) {
-        print('Error procesando evento: $e');
         return HealthMeasure(bpm: 0, spo2: 0);
       }
     }).handleError((error) {
-      print('Error en WebSocket: $error');
       return HealthMeasure(bpm: 0, spo2: 0);
     });
   }
 
-  // Cerrar la conexión
   void disconnect() {
     _channel?.sink.close();
-    print('WebSocket desconectado');
   }
 }
