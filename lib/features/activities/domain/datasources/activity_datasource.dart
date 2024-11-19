4147f8e0f77ec835047a3528a063aef9d3fd6920
steve <stev8855@gmail.com>
2024-11-19 00:06:57 -0500

    feat: Update ActivityDatasource and ActivityRepository to include activityType parameter in fetchActivities method

--- lib/features/activities/domain/datasources/activity_datasource.dart
+++ lib/features/activities/domain/datasources/activity_datasource.dart
@@ -1,5 +1,6 @@
 import 'package:guardian_area/features/activities/domain/entities/activity.dart';
 
 abstract class ActivityDatasource {
-  Future<List<Activity>> fetchActivities(String selectedDeviceRecordId);
+  Future<List<Activity>> fetchActivities(
+      String selectedDeviceRecordId, String activityType);
 }
