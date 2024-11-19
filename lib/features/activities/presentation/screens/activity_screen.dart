1926b3e8725752c8a8e38e247b4b727fd8612fa9
steve <stev8855@gmail.com>
2024-11-19 03:27:40 -0500

    feat: Update ActivityScreen to use default activity type and enhance UI with refresh functionality

--- lib/features/activities/presentation/screens/activity_screen.dart
+++ lib/features/activities/presentation/screens/activity_screen.dart
@@ -13,94 +13,111 @@ class ActivityScreen extends ConsumerStatefulWidget {
 }
 
 class _ActivityScreenState extends ConsumerState<ActivityScreen> {
-  String? selectedActivityType;
+  String selectedActivityType = 'ALL'; // Default: ALL
 
   @override
   Widget build(BuildContext context) {
-    // Obtenemos la lista de tipos de actividades
+    // ?Obtenemos el estado de carga del provider principal
+    final allActivitiesState = ref.watch(allActivitiesProvider);
+
+    // ?Obtenemos la lista de tipos de actividades
     final activityTypeState = ref.watch(activityTypeProvider);
 
-    // Obtenemos las actividades basadas en el tipo seleccionado
-    final activityState = ref.watch(
-      activityProvider(selectedActivityType ?? 'ALL'),
-    );
+    // ?Obtenemos las actividades filtradas localmente
+    final filteredActivities =
+        ref.watch(filteredActivitiesProvider(selectedActivityType));
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Activity History',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-      ),
-      body: ListView(
-        physics: const AlwaysScrollableScrollPhysics(),
+          title: const Text(
+            'Activity History',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.white),
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: activityTypeState.when(
-              data: (types) {
-                return DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.grey.shade400),
-                    borderRadius: BorderRadius.circular(8),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.shade300,
-                        blurRadius: 4,
-                        offset: const Offset(0, 2),
-                      ),
-                    ],
+          RefreshIndicator(
+            color: const Color(0xFF08273A),
+            onRefresh: () async {
+              ref.refresh(allActivitiesProvider);
+            },
+            child: ListView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              children: [
+                // Dropdown dinámico
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: activityTypeState.when(
+                    data: (types) {
+                      return DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(color: Colors.grey.shade400),
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.shade300,
+                              blurRadius: 4,
+                              offset: const Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: DropdownButtonFormField<String>(
+                            value: selectedActivityType,
+                            items: types.map((type) {
+                              return DropdownMenuItem(
+                                value: type,
+                                child: Text(type),
+                              );
+                            }).toList(),
+                            onChanged: (value) {
+                              if (value != null) {
+                                setState(() {
+                                  selectedActivityType = value;
+                                });
+                              }
+                            },
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                            ),
+                            style: const TextStyle(
+                              color: Color(0xFF08273A),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            dropdownColor: Colors.white,
+                            icon: const Icon(Icons.arrow_drop_down),
+                          ),
+                        ),
+                      );
+                    },
+                    loading: () =>
+                        const Center(child: CircularProgressIndicator()),
+                    error: (error, stackTrace) =>
+                        const Text('Failed to load activity types'),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: DropdownButtonFormField<String>(
-                      value: selectedActivityType ?? types.first,
-                      items: types.map((type) {
-                        return DropdownMenuItem(
-                          value: type,
-                          child: Text(type),
-                        );
-                      }).toList(),
-                      onChanged: (value) {
-                        if (value != null) {
-                          setState(() {
-                            selectedActivityType = value;
-                          });
-                        }
-                      },
-                      style: const TextStyle(
-                        color: Color(0xFF08273A),
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                      ),
-                      dropdownColor: Colors.white,
-                      icon: const Icon(Icons.arrow_drop_down),
-                    ),
-                  ),
-                );
-              },
-              loading: () => const Center(child: CircularProgressIndicator()),
-              error: (error, stackTrace) =>
-                  const Text('Failed to load activity types'),
+                ),
+
+                filteredActivities.isEmpty
+                    ? const Center(
+                        child: Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Text('No activities found')))
+                    : ActivityTable(activities: filteredActivities),
+              ],
             ),
           ),
-      
-          activityState.when(
-            data: (activities) {
-              if (activities.isEmpty) {
-                return const Center(child: Text('No activities found'));
-              }
-              return ActivityTable(activities: activities);
-            },
-            loading: () => const Center(child: CircularProgressIndicator()),
-            error: (error, stackTrace) =>
-                Center(child: Text('Error: $error')),
-          ),
+          if (allActivitiesState.isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.4),
+              child: const Center(
+                child: CircularProgressIndicator(
+                  color: Color(0xFF08273A),
+                ),
+              ),
+            ),
         ],
       ),
     );
