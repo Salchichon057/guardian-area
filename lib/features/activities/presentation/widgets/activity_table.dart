5250e4422f97fd9db833010d25de16ba20d87264
steve <stev8855@gmail.com>
2024-11-19 02:35:10 -0500

    feat: Add ActivityTable widget for displaying paginated activity history

---
+++ lib/features/activities/presentation/widgets/activity_table.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:guardian_area/features/activities/domain/entities/activity.dart';
+
+class ActivityTable extends StatefulWidget {
+  final List<Activity> activities;
+
+  const ActivityTable({super.key, required this.activities});
+
+  @override
+  State<ActivityTable> createState() => _ActivityTableState();
+}
+
+class _ActivityTableState extends State<ActivityTable> {
+  late final ActivityDataSource _dataSource;
+
+  @override
+  void initState() {
+    super.initState();
+    _dataSource = ActivityDataSource(widget.activities);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PaginatedDataTable(
+      header: const Text(
+        'Activity History',
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+      columns: const [
+        DataColumn(label: Text('Event')),
+        DataColumn(label: Text('Date and Time')),
+        DataColumn(label: Text('Type')),
+      ],
+      source: _dataSource,
+      rowsPerPage: 5, // Número de filas por página
+      columnSpacing: 20.0,
+      horizontalMargin: 20.0,
+      showCheckboxColumn: false,
+    );
+  }
+}
+
+class ActivityDataSource extends DataTableSource {
+  final List<Activity> activities;
+
+  ActivityDataSource(this.activities);
+
+  @override
+  DataRow getRow(int index) {
+    if (index >= activities.length) return const DataRow(cells: []);
+
+    final activity = activities[index];
+    final formattedDate =
+        DateFormat('yyyy-MM-dd HH:mm:ss').format(activity.dateAndTime);
+
+    return DataRow(cells: [
+      DataCell(Text(activity.activityName)),
+      DataCell(Text(formattedDate)),
+      DataCell(Text(activity.activityType)),
+    ]);
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => activities.length;
+
+  @override
+  int get selectedRowCount => 0;
+}
