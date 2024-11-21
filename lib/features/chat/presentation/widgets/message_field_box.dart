20fc677593596fe5b27ed7b3bb246a1d31ec96d7
steve <stev8855@gmail.com>
2024-11-21 12:05:52 -0500

    feat: Update MessageFieldBox to enhance UI and improve message submission handling

--- lib/features/chat/presentation/widgets/message_field_box.dart
+++ lib/features/chat/presentation/widgets/message_field_box.dart
@@ -1,57 +1,81 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-
   final ValueChanged<String> onValue;
-
 
   const MessageFieldBox({
     super.key,
-    required this.onValue
+    required this.onValue,
   });
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
 
     final textController = TextEditingController();
     final focusNode = FocusNode();
 
-    final outlineInputBorder = OutlineInputBorder(
-      borderSide: BorderSide(
-        color: colors.secondary,
-        width: 2,
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            blurRadius: 5,
+            offset: const Offset(0, 2),
+          ),
+        ],
       ),
-      borderRadius: BorderRadius.circular(30),
-
-    );
-
-    final inputDecoration = InputDecoration(
-      hintText: 'Escribe un mensaje',
-      enabledBorder: outlineInputBorder,
-      focusedBorder: outlineInputBorder,
-      suffixIcon: IconButton(
-        icon: const Icon(Icons.send),
-        onPressed: () {
-          final textValue = textController.value.text;
-          textController.clear();
-          onValue(textValue);
-        },
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              focusNode: focusNode,
+              controller: textController,
+              decoration: const InputDecoration(
+                  hintText: 'Type /help to see options',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 12,
+                  ),
+                  hintStyle: TextStyle(
+                    color: Color(0xFF08273A),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  )),
+              onTapOutside: (event) => focusNode.unfocus(),
+              onSubmitted: (value) {
+                textController.clear();
+                focusNode.requestFocus();
+                onValue(value);
+              },
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              final textValue = textController.value.text;
+              if (textValue.isNotEmpty) {
+                textController.clear();
+                onValue(textValue);
+              }
+            },
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration:
+                  BoxDecoration(color: colors.primary, shape: BoxShape.circle),
+              child: const Icon(
+                Icons.send,
+                color: Colors.white,
+                size: 24,
+              ),
+            ),
+          ),
+        ],
       ),
-    );
-
-    return TextFormField(
-      onTapOutside: (event) => focusNode.unfocus(),
-      focusNode: focusNode,
-      controller: textController,
-      decoration: inputDecoration,
-      onFieldSubmitted: (value) {
-        textController.clear();
-        focusNode.requestFocus(); // Enfocar el campo de texto después de enviar el mensaje
-        onValue(value);
-      },
-      // onChanged: (value) {print(value);  },
     );
   }
 }
